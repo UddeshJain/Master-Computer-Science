7e91e18bf68002106c12a0c508d9fed5c0f008e4
UddeshJain <jainuddesh35@gmail.com>
2019-07-01 15:49:11 +0530

    Implement linearSearch in JavaScript

---
+++ Algorithms/JavaScript/linearSearch.js
@@ -0,0 +1,20 @@
+/*
+    * Search an element in an array
+    * @param {*} array - an array of elements
+    * @param {*} item - an element to search
+    */
+function linearSearch(array, item) {
+    let index = -1   // Default case i.e Item not found
+    array.forEach((element, i) => {
+        if (element === item) {
+            index = i
+        }
+
+    });
+    if (index === -1) {
+        return `${item} is not found`
+    }
+    return `${item} found at index ${index}`
+}
+
+console.log(linearSearch([1,4,5,6,7,3,9,13,65,34,77,43], 77))

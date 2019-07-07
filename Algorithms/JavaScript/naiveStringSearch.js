f61edacef1fae4d5e886868f794f4c15ffaf8ec6
UddeshJain <jainuddesh35@gmail.com>
2019-07-07 23:31:49 +0530

    Syntax optimization

--- Algorithms/JavaScript/naiveStringSearch.js
+++ Algorithms/JavaScript/naiveStringSearch.js
@@ -2,17 +2,11 @@
 function naiveSearch(longStr, shortStr) {
     let counter = 0
     for (let i = 0; i < longStr.length; i++) {
-        
         for (let j = 0; j < shortStr.length; j++) {
-            if (shortStr[j] !== longStr[i + j]) {
-                break
-            }
-            if (j === shortStr.length - 1) {
-                counter = counter + 1
-            }
+            if (shortStr[j] !== longStr[i + j]) break
+            if (j === shortStr.length - 1) counter = counter + 1
         }
     }
     return counter
 }
-const longStr = 'how are you'
 console.log(naiveSearch("how are you", "are"))

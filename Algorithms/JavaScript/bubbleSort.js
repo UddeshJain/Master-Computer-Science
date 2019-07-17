189348e7017acaf8fea6abb41feb03d6b3fc2016
UddeshJain <jainuddesh35@gmail.com>
2019-07-17 16:13:00 +0530

    Added some helpful comments

--- Algorithms/JavaScript/bubbleSort.js
+++ Algorithms/JavaScript/bubbleSort.js
@@ -1,3 +1,7 @@
+/*
+    * bubbleSort - to sort a given array
+    * @param {array} arr - Array to sort
+*/
 function bubbleSort(arr) {
     for (let i = arr.length; i > 0; i--) {
         for (let j = 0; j < i-1; j++) {

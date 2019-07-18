d84cfb295676c6d3df8ac4051eef288be4d2970c
UddeshJain <jainuddesh35@gmail.com>
2019-07-18 12:26:05 +0530

    Performance improvement

--- Algorithms/JavaScript/bubbleSort.js
+++ Algorithms/JavaScript/bubbleSort.js
@@ -3,14 +3,18 @@
     * @param {array} arr - Array to sort
 */
 function bubbleSort(arr) {
+    var isSwapping
     for (let i = arr.length; i > 0; i--) {
+        isSwapping = true
         for (let j = 0; j < i-1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j]
                 arr[j] = arr[j + 1]
-                arr[j+1] = temp
+                arr[j + 1] = temp
+                isSwapping = false
             }
         }
+        if(isSwapping) break    // For optimization(exit from loop if there is nothing to swap)
     }
     return arr
 }

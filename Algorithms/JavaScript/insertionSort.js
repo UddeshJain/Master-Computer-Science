ce222fc94812105c4d87060805e7ba21856bc234
UddeshJain <jainuddesh35@gmail.com>
2019-08-13 13:43:04 +0530

    Reformed insertion sort algorithm.

--- Algorithms/JavaScript/insertionSort.js
+++ Algorithms/JavaScript/insertionSort.js
@@ -3,18 +3,12 @@
     * @param {array} arr - Array to sort
 */
 function insertionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let lowest = i
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[lowest] > arr[j]) {
-                lowest = j
-            }
+    for (let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i]
+        for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
         }
-        if (lowest !== i) {
-            let temp = arr[i]
-            arr[i] = arr[lowest]
-            arr[lowest] = temp   
-        }
+        arr[j+1] = currentVal
     }
     return arr
 }

9c4a607c722649f8c94cfa5d93821363b5f86a3c
UddeshJain <jainuddesh35@gmail.com>
2019-08-23 15:58:08 +0530

    Implemented merging part of mergeSort algorithm

---
+++ Algorithms/JavaScript/mergeSort.js
@@ -0,0 +1,28 @@
+/*
+    * merge - to merge two sorted array
+    * @param {arr1, arr2} array - Two Array to merge
+*/
+function merge(arr1, arr2) {
+    let result = []
+    let i = 0
+    let j = 0
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i])
+            i++
+        } else {
+            result.push(arr2[j])
+            j++
+        }
+    }
+    while (i < arr1.length) {
+        result.push(arr1[i])
+        i++
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j])
+        j++
+    }
+    return result
+}
+console.log(merge([1,2,5], [3,4,6,8,9]))
